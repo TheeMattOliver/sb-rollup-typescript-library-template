5e6c23fe3d27d795c67afa2fc7162554fbb16441
Matt Oliver <mattoliver.mattoliver@gmail.com>
2022-07-31 09:36:34 -0500

    set up sb

--- src/Thing.tsx
+++ src/Thing.tsx
@@ -3,6 +3,7 @@ import React from "react";
 type ThingProps = {
   children: React.ReactNode;
 };
+
 export const Thing = ({ children }: ThingProps) => {
   return <div>{children}</div>;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export { default as Thing } from "./Thing";
+export { Thing } from "./Thing";
